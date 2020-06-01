681258c72368691dd0043bf8bdf3e69609535dea
korey ko <koreykendrick1@gmail.com>
2020-06-01 17:12:50 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,6 +7,11 @@ block_return_values = []
  block_return_values << yield(collection[i])
     i = i + 1 
   end
-  
+   if block_return_values.include?(false)
+    false
+  else
+    true
+  end
+end
 end
 
